042aec09961409f4d5989201957b7a5445e852a9
Ismael Correia <ismaelavilacorreia@gmail.com>
2022-04-06 14:20:45 -0300

    build: building initial project structure and installing frameworks / modules

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import express from "express";
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Rendering!");
+});
+
+app.listen(port, () => {
+  console.log(`Running on port ${port}`);
+});
