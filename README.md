0f755ef7e8c7d603feef6c0ec3caadc1e46d9388
Ismael Correia <ismaelavilacorreia@gmail.com>
2022-04-06 15:25:44 -0300

    build: creating database file

---
+++ README.md
@@ -0,0 +1,3 @@
+Project: User CRUD and Admin permissions
+
+This repository was created as a Kenzie Academy study practice repository and does not represent a complete work/project in anyway.
--- src/app.js
+++ src/app.js
@@ -9,3 +9,13 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Running on port ${port}`);
 });
+
+/* 
+Endpoints
+    POST /users = criação de usuário
+    POST /login = gera um token recebendo email e password
+    GET /users = lista todos os usuários
+    GET /users/profile = retorna os dados do usuário logado
+    PATCH /users/:id = atualiza os dados de um usuário
+    DELETE /users/:id = deleta usuários do banco
+*/
